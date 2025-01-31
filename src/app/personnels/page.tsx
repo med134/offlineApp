79bb1e6c3c5655c359c3e6d569eb0b442a19f0c8
med134 <81263260+med134@users.noreply.github.com>
2025-01-31 23:07:52 +0100

    persole

--- src/app/personnels/page.tsx
+++ src/app/personnels/page.tsx
@@ -1,45 +1,11 @@
-"use client";
-import { Personnel } from "@/components/Interface";
-import PersonnelTable from "@/components/PersonnelTable";
-import { pouchDB } from "@/utils/Poush";
-import React, { useState, useEffect } from "react";
-
-const Page = () => {
-  const [personnels, setPersonnels] = useState<Personnel[]>([]); // Ensure state is an array
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await pouchDB.allDocs({ include_docs: true });
-
-        // Ensure personnelList is correctly typed
-        const personnelList: Personnel[] = result.rows
-          .map((row) => row.doc as Personnel)
-          .filter((doc) => !!doc); // Remove undefined/null values
-
-        setPersonnels(personnelList);
-      } catch (err) {
-        console.error("Error fetching documents:", err);
-      }
-    };
-    fetchData();
-  }, []);
-
-  // Log updated state
-  useEffect(() => {
-    console.log(personnels);
-  }, [personnels]);
-
+import PersonnalPage from '@/components/PersonnalPage'
+import React from 'react'
+const page = () => {
   return (
-    <div className="py-16">
-      <h1 className="text-2xl py-6 px-6 font-sans font-semibold uppercase">
-        Les informations des agents de PC Skhour Rhamna
-      </h1>
-      {personnels?.map((personnel) => (
-        <PersonnelTable key={personnel._id} personnel={personnel} />
-      ))}
+    <div>
+      <PersonnalPage/>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
